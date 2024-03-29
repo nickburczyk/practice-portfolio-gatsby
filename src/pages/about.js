2dfa06c5502fb7e41c7aed3bbf4a296f79d2fbe0
nickburczyk <nick.burczyk@gmail.com>
2024-03-29 09:01:39 +0100

    Add profile pic to about page

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,11 +1,13 @@
 import * as React from 'react'
 import { Link } from 'gatsby'
 import Layout from '../components/Layout/layout'
+import { StaticImage } from 'gatsby-plugin-image'
 
 const AboutPage = () => {
   return (
     <Layout pageTitle="About Me">
         <p>Hi there! I'm the proud creator of this site, which I built with Gatsby.</p>
+        <StaticImage src="../images/profile.jpg" alt="A picture of an alien that kind of looks like me"
     </Layout>
   )
 }
